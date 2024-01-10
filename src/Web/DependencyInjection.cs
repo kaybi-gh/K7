ab6a015766abe7fc4e99928e7f75a5546fa56f52
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2024-01-10 23:53:19 +0100

    feat: added --init arg to init database only when asked

--- src/Web/DependencyInjection.cs
+++ src/Web/DependencyInjection.cs
@@ -13,16 +13,13 @@ public static class DependencyInjection
     public static IServiceCollection AddWebServices(this IServiceCollection services)
     {
         services.AddDatabaseDeveloperPageExceptionFilter();
-
         services.AddScoped<IUser, CurrentUser>();
-
         services.AddHttpContextAccessor();
 
         services.AddHealthChecks()
             .AddDbContextCheck<ApplicationDbContext>();
 
         services.AddExceptionHandler<CustomExceptionHandler>();
-
         services.AddRazorPages();
 
         services.AddScoped(provider =>
