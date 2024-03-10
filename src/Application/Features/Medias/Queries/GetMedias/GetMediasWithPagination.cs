0865ee2d078631df8eb1a6dffc6930cdcef48a24
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2024-03-10 22:12:21 +0100

    feat: initial GetMedias query feature implementation

---
+++ src/Application/Features/Medias/Queries/GetMedias/GetMediasWithPagination.cs
@@ -0,0 +1,31 @@
+﻿using MediaServer.Application.Common.Interfaces;
+using MediaServer.Application.Common.Mappings;
+using MediaServer.Application.Common.Models;
+
+namespace MediaServer.Application.Features.Medias.Queries.GetMedias;
+
+public record GetMediasWithPaginationQuery : IRequest<PaginatedList<MediaDto>>
+{
+    public int? LibraryId { get; init; }
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+}
+
+public class GetMediasQueryHandler : IRequestHandler<GetMediasWithPaginationQuery, PaginatedList<MediaDto>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetMediasQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedList<MediaDto>> Handle(GetMediasWithPaginationQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Medias
+            .ProjectTo<MediaDto>(_mapper.ConfigurationProvider)
+            .PaginatedListAsync(request.PageNumber, request.PageSize);
+    }
+}
---
+++ src/Application/Features/Medias/Queries/GetMedias/MediaDto.cs
@@ -0,0 +1,14 @@
+﻿using MediaServer.Domain.Entities.Medias;
+
+namespace MediaServer.Application.Features.Medias.Queries.GetMedias;
+
+public record MediaDto
+{
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<BaseMedia, MediaDto>();
+        }
+    }
+}
