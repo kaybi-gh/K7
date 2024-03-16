77db0f90157fba2b4bcbbb17c16138892a620022
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2024-03-16 20:47:06 +0100

    feat: implemented initial GetMediaPicture

--- src/Application/Features/Medias/Queries/GetMedias/MediaDto.cs
+++ src/Application/Features/Medias/Queries/GetMedias/MediaDto.cs
@@ -34,12 +34,14 @@ public record PictureDto
 {
     public int Id { get; init; }
     public MediaPictureType? Type { get; init; }
+    public Uri? Uri { get; init; }
 
     private class Mapping : Profile
     {
         public Mapping()
         {
-            CreateMap<MediaPicture, PictureDto>();
+            CreateMap<MediaPicture, PictureDto>()
+                .ForMember(dest => dest.Uri, x => x.MapFrom(src => new Uri($"/api/pictures/{src.Id}", UriKind.Relative)));
         }
     }
 }
---
+++ src/Web/Endpoints/MediaPictures.cs
@@ -0,0 +1,19 @@
+﻿using MediaServer.Application.Features.Libraries.Queries.GetMediaPicture;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaServer.Web.Endpoints;
+
+public class Pictures : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            //.RequireAuthorization()
+            .MapGet(GetMediaPicture, "{id}");
+    }
+
+    public async Task<IResult> GetMediaPicture(ISender sender, [FromRoute] int id)
+    {
+        return await sender.Send(new GetMediaPictureQuery(id));
+    }
+}
