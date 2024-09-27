a3d6da5bc84244592496af5dbc3c032492390692
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2024-09-27 23:52:57 +0200

    feat: progress on hls streams

--- src/Application/Features/IndexedFiles/Queries/GetHlsStreamSegment/GetHlsStreamSegment.cs
+++ src/Application/Features/IndexedFiles/Queries/GetHlsStreamSegment/GetHlsStreamSegment.cs
@@ -1,12 +1,26 @@
-﻿using System.Text;
-using MediaServer.Application.Common.Interfaces;
+﻿using MediaServer.Application.Common.Interfaces;
 using MediaServer.Infrastructure.Configuration;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 
 namespace MediaServer.Application.Features.IndexedFiles.Queries.GetHlsStreamSegment;
 
-public record GetHlsStreamSegmentQuery(Guid Id, string VideoResolutionIdentifier, int segmentId) : IRequest<IResult>;
+public static class GetHlsStreamSegmentQueryUriBuilder
+{
+    public const string Route = "{id}/hls-stream/{quality}/segments/{segmentId}.ts";
+
+    public static string Build(GetHlsStreamSegmentQuery query) => Route
+        .Replace("{id}", $"{query.Id}")
+        .Replace("{segmentId}", $"{query.SegmentId}")
+        .Replace("{quality}", query.VideoResolutionIdentifier);
+
+    public static string Build(Guid id, string videoResolutionIdentifier, int segmentId) => Route
+        .Replace("{id}", $"{id}")
+        .Replace("{segmentId}", $"{segmentId}")
+        .Replace("{quality}", videoResolutionIdentifier);
+}
+
+public record GetHlsStreamSegmentQuery(Guid Id, string VideoResolutionIdentifier, int SegmentId) : IRequest<IResult>;
 
 public class GetHlsStreamSegmentQueryHandler : IRequestHandler<GetHlsStreamSegmentQuery, IResult>
 {
@@ -21,7 +35,7 @@ public class GetHlsStreamSegmentQueryHandler : IRequestHandler<GetHlsStreamSegme
 
     public async Task<IResult> Handle(GetHlsStreamSegmentQuery query, CancellationToken cancellationToken)
     {
-        var segmentPath = Path.Combine(_pathsConfiguration.Transcoding, $"{query.Id}", query.VideoResolutionIdentifier, $"{query.segmentId}.ts");
+        var segmentPath = Path.Combine(_pathsConfiguration.Transcoding, $"{query.Id}", query.VideoResolutionIdentifier, $"{query.SegmentId}.ts");
         var file = new FileInfo(segmentPath);
 
         if (file.Exists)
@@ -34,7 +48,7 @@ public class GetHlsStreamSegmentQueryHandler : IRequestHandler<GetHlsStreamSegme
 
         var segments = await _context.HlsSegments
             .Where(x => x.IndexedFileId == query.Id)
-            .Where(x => x.Number >= query.segmentId)
+            .Where(x => x.Number >= query.SegmentId)
             .Take(10)
             .ToListAsync();
 
