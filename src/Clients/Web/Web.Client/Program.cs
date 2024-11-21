2be63ab217a7ee0432c11bb8d2392c1c9bf32a53
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2024-11-21 23:36:19 +0100

    feat: use new static assets mapping and update hybrid blazor stuff

--- src/Clients/Web/Web.Client/Program.cs
+++ src/Clients/Web/Web.Client/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using K7.Clients.Shared.Services;
+using K7.Clients.Shared.Domain.Interfaces;
+using K7.Clients.Web.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -8,5 +10,6 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddMudServices();
 builder.Services.AddConfigurations(builder.Configuration);
 builder.Services.AddClientServices();
+builder.Services.AddSingleton<IFormFactorService, FormFactorService>();
 
 await builder.Build().RunAsync();
