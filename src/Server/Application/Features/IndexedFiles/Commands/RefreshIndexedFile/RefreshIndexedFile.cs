ff3701154f27ae13c4ad6127f4f34ad03aa6f6e6
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2026-03-05 22:47:28 +0100

    feat: better indexed file refreshing

--- src/Server/Application/Features/IndexedFiles/Commands/RefreshIndexedFile/RefreshIndexedFile.cs
+++ src/Server/Application/Features/IndexedFiles/Commands/RefreshIndexedFile/RefreshIndexedFile.cs
@@ -1,7 +1,6 @@
 using K7.Server.Application.Common.Interfaces;
 using K7.Server.Domain.Enums;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
+using K7.Server.Domain.Events;
 
 namespace K7.Server.Application.Features.IndexedFiles.Commands.RefreshIndexedFile;
 
@@ -10,12 +9,10 @@ public record RefreshIndexedFileCommand(Guid Id) : IRequest;
 public class RefreshIndexedFileCommandHandler : IRequestHandler<RefreshIndexedFileCommand>
 {
     private readonly IApplicationDbContext _context;
-    private readonly ISender _sender;
 
     public RefreshIndexedFileCommandHandler(IApplicationDbContext context, ISender sender)
     {
         _context = context;
-        _sender = sender;
     }
 
     public async Task Handle(RefreshIndexedFileCommand request, CancellationToken cancellationToken)
@@ -30,24 +27,14 @@ public class RefreshIndexedFileCommandHandler : IRequestHandler<RefreshIndexedFi
         var library = await _context.Libraries.FindAsync([indexedFile.LibraryId], cancellationToken);
         Guard.Against.NotFound(indexedFile.LibraryId, library);
 
-        FileType fileType = library.MediaType switch
+        indexedFile.AddDomainEvent(new IndexedFileCreatedEvent(indexedFile, library.MediaType switch
         {
             LibraryMediaType.Movie => FileType.Video,
-            LibraryMediaType.Serie => FileType.Video,
             LibraryMediaType.Music => FileType.Audio,
-            _ => throw new NotImplementedException()
-        };
+            LibraryMediaType.Serie => FileType.Video,
+            _ => throw new InvalidOperationException(),
+        }));
 
-        await _sender.Send(new BackgroundTasks.Commands.CreateBackgroundTask.CreateBackgroundTaskCommand()
-        {
-            Request = new CreateFileMetadatas.CreateFileMetadatasCommand()
-            {
-                Id = indexedFile.Id,
-                FileType = fileType
-            },
-            Priority = BackgroundTaskPriority.High,
-            TargetEntityId = indexedFile.Id,
-            TargetEntityTypeName = nameof(Domain.Entities.IndexedFile)
-        }, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
     }
 }
