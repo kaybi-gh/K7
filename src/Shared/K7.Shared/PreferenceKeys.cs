ccd46d39af180e82d83f81a1b4c6dc8ed2e59ef2
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2026-03-20 23:57:35 +0100

    feat(setup): validate server URL and cache server info

--- src/Shared/K7.Shared/PreferenceKeys.cs
+++ src/Shared/K7.Shared/PreferenceKeys.cs
@@ -14,4 +14,7 @@ public static class PreferenceKeys
     public static readonly PreferenceKey<bool> PLAYER_IS_MUTED = new("PlayerIsMuted");
     public static readonly PreferenceKey<bool> PLAYER_ADAPTIVE_CROSSFADE = new("PlayerAdaptiveCrossfade");
     public static readonly PreferenceKey<double> PLAYER_CROSSFADE_DURATION = new("PlayerCrossfadeDuration");
+    public static readonly PreferenceKey<string> ACCESS_TOKEN = new("AccessToken");
+    public static readonly PreferenceKey<string> REFRESH_TOKEN = new("RefreshToken");
+    public static readonly PreferenceKey<string> SERVER_INFO = new("ServerInfo");
 }
