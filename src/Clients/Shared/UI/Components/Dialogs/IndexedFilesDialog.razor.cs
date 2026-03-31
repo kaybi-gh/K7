3ed11ee9070d50ce1225ae80f98d2d822d634b67
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2026-03-31 21:50:02 +0200

    feat(ui): add enriched indexed files detail dialog

---
+++ src/Clients/Shared/UI/Components/Dialogs/IndexedFilesDialog.razor.cs
@@ -0,0 +1,31 @@
+using K7.Shared.Dtos.Entities.Medias;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace K7.Clients.Shared.UI.Components.Dialogs;
+
+public partial class IndexedFilesDialog
+{
+    [CascadingParameter]
+    IMudDialogInstance MudDialog { get; set; } = default!;
+
+    [Parameter]
+    public required MovieDto Movie { get; set; }
+
+    [Parameter]
+    public EventCallback<Guid> OnReIdentifyFile { get; set; }
+
+    private void Cancel()
+    {
+        MudDialog.Cancel();
+    }
+
+    private async Task ReIdentifyFile(Guid fileId)
+    {
+        if (OnReIdentifyFile.HasDelegate)
+        {
+            await OnReIdentifyFile.InvokeAsync(fileId);
+        }
+        MudDialog.Close(DialogResult.Ok(true));
+    }
+}
