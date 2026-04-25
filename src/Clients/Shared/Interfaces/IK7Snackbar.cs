97ee1ef73f0ecfa589b881961b68d0cf2afdb00f
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2026-04-25 19:31:35 +0200

    fix(client): expose OnAdd event directly on IK7Snackbar interface

--- src/Clients/Shared/Interfaces/IK7Snackbar.cs
+++ src/Clients/Shared/Interfaces/IK7Snackbar.cs
@@ -1,8 +1,11 @@
 using K7.Clients.Shared.Models;
+using K7.Clients.Shared.Services;
 
 namespace K7.Clients.Shared.Interfaces;
 
 public interface IK7Snackbar
 {
+    event Action<K7SnackbarMessage>? OnAdd;
+
     void Add(string message, K7Severity severity = K7Severity.Normal, string? title = null);
 }
