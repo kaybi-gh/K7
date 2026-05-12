005b9b9514361d4d938c4a5da4a74c33fe2263f4
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2026-05-12 18:59:34 +0200

    perf(home): optimize home feed query performance and add caching

--- src/Server/Web/Endpoints/Home/GetHomeFeed.cs
+++ src/Server/Web/Endpoints/Home/GetHomeFeed.cs
@@ -1,7 +1,9 @@
+using K7.Server.Application.Common.Interfaces;
 using K7.Server.Application.Features.Home.Queries.GetHomeFeedItems;
 using K7.Server.Domain.Constants;
 using K7.Server.Web.Converters;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 
 namespace K7.Server.Web.Endpoints.Home;
 
@@ -13,10 +15,19 @@ public class GetHomeFeed : IEndpoint
         var groupName = type.Namespace!.Split('.').Last();
 
         endpointRouteBuilder.MapGet("/api/home/feed", async (
+            HttpContext httpContext,
             [FromServices] ISender sender,
+            [FromServices] IMediaQueryCacheInvalidator cacheInvalidator,
             [AsParameters] GetHomeFeedItemsQuery query) =>
         {
+            var etag = $"\"{cacheInvalidator.Version}\"";
+
+            if (httpContext.Request.Headers.IfNoneMatch == etag)
+                return Results.StatusCode(StatusCodes.Status304NotModified);
+
             var result = await sender.Send(query);
+
+            httpContext.Response.Headers[HeaderNames.ETag] = etag;
             return Results.Ok(result.ToDto(x => x));
         })
         .RequireAuthorization(Policies.GuestOrAbove)
