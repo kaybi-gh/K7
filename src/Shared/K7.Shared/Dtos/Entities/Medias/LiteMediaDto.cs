7e0fec0e1d5ee48bec9da3f0757ab78d2bc6c26a
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2026-05-12 18:18:58 +0200

    feat(shared): add SortName to LiteMediaDto

--- src/Shared/K7.Shared/Dtos/Entities/Medias/LiteMediaDto.cs
+++ src/Shared/K7.Shared/Dtos/Entities/Medias/LiteMediaDto.cs
@@ -14,6 +14,7 @@ public abstract record LiteMediaDto
     public Guid Id { get; init; }
     public string? Title { get; init; }
     public DateOnly? ReleaseDate { get; init; }
+    public DateTimeOffset? Created { get; init; }
     public IReadOnlyList<MetadataPictureDto>? Pictures { get; init; }
     public UserMediaStateDto? UserState { get; init; }
     public int? UserRating { get; init; }
