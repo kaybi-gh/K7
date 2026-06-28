3ec9b973856a79978dbd17071ca15c7fef0b83d7
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2026-06-28 19:11:07 +0200

    feat(diagnostics): add helpers to sum issues by library, type and entity

--- src/Shared/K7.Shared/Dtos/Diagnostics/LibraryHealthSummaryCounts.cs
+++ src/Shared/K7.Shared/Dtos/Diagnostics/LibraryHealthSummaryCounts.cs
@@ -1,3 +1,5 @@
+using K7.Server.Domain.Enums;
+
 namespace K7.Shared.Dtos.Diagnostics;
 
 public static class LibraryHealthSummaryCounts
@@ -15,4 +17,43 @@ public static class LibraryHealthSummaryCounts
 
     public static int SumTotal(IEnumerable<LibraryHealthSummaryDto> summaries) =>
         SumErrors(summaries) + SumWarnings(summaries) + SumInfo(summaries);
+
+    public static int SumLibraryIssues(LibraryHealthSummaryDto summary) =>
+        summary.OrphanIndexedFileCount + summary.UnidentifiedIndexedFileCount + summary.MissingFileMetadataCount
+        + summary.MissingHlsSegmentsCount + summary.MediaMissingPicturesCount + summary.MediaMissingExternalIdCount
+        + summary.MediaMissingMetadataCount + summary.MediaWithoutFilesCount + summary.StaleMetadataCount
+        + summary.MissingAudioAnalysisCount + summary.InaccessiblePathCount;
+
+    public static int SumIssue(IEnumerable<LibraryHealthSummaryDto> summaries, DiagnosticIssue issue) =>
+        summaries.Sum(s => CountIssue(s, issue));
+
+    public static int SumEntityType(IEnumerable<LibraryHealthSummaryDto> summaries, DiagnosticEntityType entityType) =>
+        summaries.Sum(s => CountEntityType(s, entityType));
+
+    private static int CountIssue(LibraryHealthSummaryDto summary, DiagnosticIssue issue) => issue switch
+    {
+        DiagnosticIssue.OrphanFile => summary.OrphanIndexedFileCount,
+        DiagnosticIssue.UnidentifiedFile => summary.UnidentifiedIndexedFileCount,
+        DiagnosticIssue.MissingFileMetadata => summary.MissingFileMetadataCount,
+        DiagnosticIssue.MissingHlsSegments => summary.MissingHlsSegmentsCount,
+        DiagnosticIssue.MissingPictures => summary.MediaMissingPicturesCount,
+        DiagnosticIssue.MissingMetadata => summary.MediaMissingMetadataCount,
+        DiagnosticIssue.MissingExternalId => summary.MediaMissingExternalIdCount,
+        DiagnosticIssue.StaleMetadata => summary.StaleMetadataCount,
+        DiagnosticIssue.MissingAudioAnalysis => summary.MissingAudioAnalysisCount,
+        DiagnosticIssue.MissingFiles => summary.MediaWithoutFilesCount,
+        DiagnosticIssue.InaccessiblePath => summary.InaccessiblePathCount,
+        _ => 0
+    };
+
+    private static int CountEntityType(LibraryHealthSummaryDto summary, DiagnosticEntityType entityType) => entityType switch
+    {
+        DiagnosticEntityType.IndexedFile => summary.OrphanIndexedFileCount + summary.UnidentifiedIndexedFileCount
+            + summary.MissingFileMetadataCount + summary.MissingHlsSegmentsCount,
+        DiagnosticEntityType.Media => summary.MediaMissingPicturesCount + summary.MediaMissingExternalIdCount
+            + summary.MediaMissingMetadataCount + summary.MediaWithoutFilesCount + summary.StaleMetadataCount
+            + summary.MissingAudioAnalysisCount,
+        DiagnosticEntityType.Library => summary.InaccessiblePathCount,
+        _ => 0
+    };
 }
