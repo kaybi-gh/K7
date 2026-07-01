1e4bf62432728055f2c7e9a1c847edd2094ce11a
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2026-07-01 22:19:56 +0200

    refactor(media): remove unused media format sample endpoint

--- src/Server/Domain/Interfaces/IMediaFormatSampleGenerator.cs
+++
@@ -1,8 +0,0 @@
-﻿using K7.Server.Domain.Entities.MediaFormats;
-
-namespace K7.Server.Domain.Interfaces;
-
-public interface IMediaFormatSampleGenerator
-{
-    Task<MemoryStream> GenerateSampleAsync(BaseMediaFormat mediaFormat);
-}
