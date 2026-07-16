5b291a92fb9a4cf3c52b2c8097ad45d78184693d
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2026-07-16 18:16:29 +0200

    test(application): cover path containment and string parsing

---
+++ tests/Application.UnitTests/Helpers/PathContainmentHelperTests.cs
@@ -0,0 +1,73 @@
+using K7.Server.Application.Common.Security;
+
+namespace K7.Server.Application.UnitTests.Helpers;
+
+public class PathContainmentHelperTests
+{
+    [Test]
+    public void IsPathContained_ShouldReturnTrue_ForExactRootAndChild()
+    {
+        var root = Path.GetFullPath(Path.Combine(Path.GetTempPath(), "k7-root-" + Guid.NewGuid().ToString("N")));
+        Directory.CreateDirectory(root);
+        try
+        {
+            var child = Path.Combine(root, "movies", "a.mkv");
+
+            PathContainmentHelper.IsPathContained(root, [root]).Should().BeTrue();
+            PathContainmentHelper.IsPathContained(child, [root]).Should().BeTrue();
+        }
+        finally
+        {
+            Directory.Delete(root, recursive: true);
+        }
+    }
+
+    [Test]
+    public void IsPathContained_ShouldReturnFalse_WhenOutsideAllowedRoots()
+    {
+        var root = Path.GetFullPath(Path.Combine(Path.GetTempPath(), "k7-root-" + Guid.NewGuid().ToString("N")));
+        var other = Path.GetFullPath(Path.Combine(Path.GetTempPath(), "k7-other-" + Guid.NewGuid().ToString("N")));
+        Directory.CreateDirectory(root);
+        Directory.CreateDirectory(other);
+        try
+        {
+            PathContainmentHelper.IsPathContained(Path.Combine(other, "x.mkv"), [root]).Should().BeFalse();
+        }
+        finally
+        {
+            Directory.Delete(root, recursive: true);
+            Directory.Delete(other, recursive: true);
+        }
+    }
+
+    [Test]
+    public void IsPathContained_ShouldIgnoreBlankRoots()
+    {
+        var candidate = Path.GetTempPath();
+
+        PathContainmentHelper.IsPathContained(candidate, ["", "  ", null!]).Should().BeFalse();
+    }
+
+    [Test]
+    public void EnsurePathContained_ShouldThrow_WhenNotContained()
+    {
+        var root = Path.GetFullPath(Path.Combine(Path.GetTempPath(), "k7-root-" + Guid.NewGuid().ToString("N")));
+        var other = Path.GetFullPath(Path.Combine(Path.GetTempPath(), "k7-other-" + Guid.NewGuid().ToString("N")));
+        Directory.CreateDirectory(root);
+        Directory.CreateDirectory(other);
+        try
+        {
+            var act = () => PathContainmentHelper.EnsurePathContained(
+                Path.Combine(other, "x.mkv"),
+                [root],
+                "denied");
+
+            act.Should().Throw<UnauthorizedAccessException>().WithMessage("denied");
+        }
+        finally
+        {
+            Directory.Delete(root, recursive: true);
+            Directory.Delete(other, recursive: true);
+        }
+    }
+}
