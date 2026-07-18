3db0a691156b74f89fc8c36936c82b069df9b6b2
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2026-07-18 20:19:06 +0200

    test(path): split pathhelper cases for win and unix

--- tests/Application.UnitTests/Helpers/PathHelperTests.cs
+++ tests/Application.UnitTests/Helpers/PathHelperTests.cs
@@ -5,7 +5,8 @@ namespace K7.Server.Application.UnitTests.Helpers;
 public class PathHelperTests
 {
     [Test]
-    public void IsPathInScope_ShouldMatchExactFilePath()
+    [Platform("Win")]
+    public void IsPathInScope_ShouldMatchExactFilePath_Windows()
     {
         var file = @"C:\music\artist\album\01-track.flac";
         var scope = @"C:\music\artist\album\01-track.flac";
@@ -14,7 +15,18 @@ public class PathHelperTests
     }
 
     [Test]
-    public void IsPathInScope_ShouldMatchNestedFileUnderDirectoryScope()
+    [Platform("Unix")]
+    public void IsPathInScope_ShouldMatchExactFilePath_Unix()
+    {
+        var file = "/music/artist/album/01-track.flac";
+        var scope = "/music/artist/album/01-track.flac";
+
+        PathHelper.IsPathInScope(file, scope).Should().BeTrue();
+    }
+
+    [Test]
+    [Platform("Win")]
+    public void IsPathInScope_ShouldMatchNestedFileUnderDirectoryScope_Windows()
     {
         var file = @"C:\music\artist\album\01-track.flac";
         var scope = @"C:\music\artist\album";
@@ -23,7 +35,18 @@ public class PathHelperTests
     }
 
     [Test]
-    public void IsPathInScope_ShouldNotMatchSiblingDirectoryPrefix()
+    [Platform("Unix")]
+    public void IsPathInScope_ShouldMatchNestedFileUnderDirectoryScope_Unix()
+    {
+        var file = "/music/artist/album/01-track.flac";
+        var scope = "/music/artist/album";
+
+        PathHelper.IsPathInScope(file, scope).Should().BeTrue();
+    }
+
+    [Test]
+    [Platform("Win")]
+    public void IsPathInScope_ShouldNotMatchSiblingDirectoryPrefix_Windows()
     {
         var file = @"C:\music\artist\album-extra\01-track.flac";
         var scope = @"C:\music\artist\album";
@@ -32,7 +55,18 @@ public class PathHelperTests
     }
 
     [Test]
-    public void IsPathInScope_ShouldTreatMixedSeparatorsAsEqual()
+    [Platform("Unix")]
+    public void IsPathInScope_ShouldNotMatchSiblingDirectoryPrefix_Unix()
+    {
+        var file = "/music/artist/album-extra/01-track.flac";
+        var scope = "/music/artist/album";
+
+        PathHelper.IsPathInScope(file, scope).Should().BeFalse();
+    }
+
+    [Test]
+    [Platform("Win")]
+    public void IsPathInScope_ShouldTreatMixedSeparatorsAsEqual_Windows()
     {
         var file = @"C:/music/artist/album/01-track.flac";
         var scope = @"C:\music\artist\album\01-track.flac";
@@ -41,7 +75,8 @@ public class PathHelperTests
     }
 
     [Test]
-    public void NormalizeLibraryPath_ShouldResolveRelativePathAgainstLibraryRoot()
+    [Platform("Win")]
+    public void NormalizeLibraryPath_ShouldResolveRelativePathAgainstLibraryRoot_Windows()
     {
         var normalized = PathHelper.NormalizeLibraryPath(@"artist\album\track.flac", @"C:\music");
 
@@ -49,10 +84,30 @@ public class PathHelperTests
     }
 
     [Test]
-    public void NormalizeLibraryPath_ShouldKeepAbsolutePathUnchanged()
+    [Platform("Unix")]
+    public void NormalizeLibraryPath_ShouldResolveRelativePathAgainstLibraryRoot_Unix()
+    {
+        var normalized = PathHelper.NormalizeLibraryPath("artist/album/track.flac", "/music");
+
+        normalized.Should().Be(PathHelper.NormalizePath("/music/artist/album/track.flac"));
+    }
+
+    [Test]
+    [Platform("Win")]
+    public void NormalizeLibraryPath_ShouldKeepAbsolutePathUnchanged_Windows()
     {
         var path = @"C:\music\artist\album\track.flac";
         var normalized = PathHelper.NormalizeLibraryPath(path, @"D:\other-root");
+
+        normalized.Should().Be(PathHelper.NormalizePath(path));
+    }
+
+    [Test]
+    [Platform("Unix")]
+    public void NormalizeLibraryPath_ShouldKeepAbsolutePathUnchanged_Unix()
+    {
+        var path = "/music/artist/album/track.flac";
+        var normalized = PathHelper.NormalizeLibraryPath(path, "/other-root");
 
         normalized.Should().Be(PathHelper.NormalizePath(path));
     }
