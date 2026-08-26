acaba75d32fd5399a0b6f8b3fba74e31a575f83c
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2026-08-26 22:41:31 +0200

    fix(import): bump system.commandline to 2.0.11

--- tools/K7.Import/Program.cs
+++ tools/K7.Import/Program.cs
@@ -1,7 +1,4 @@
-using System.CommandLine;
 using K7.Import.Commands;
 
 var rootCommand = ImportCommand.CreateRoot();
-
-var config = new CommandLineConfiguration(rootCommand);
-return await config.InvokeAsync(args);
+return await rootCommand.Parse(args).InvokeAsync();
