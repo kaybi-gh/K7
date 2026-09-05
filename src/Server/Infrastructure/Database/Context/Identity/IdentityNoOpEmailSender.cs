e33a4fa2edb5ae6f87b11a54505a101fdcc9b35e
kaybi-gh <13943086+kaybi-gh@users.noreply.github.com>
2026-09-05 14:16:14 +0200

    chore(deps): land safe bumps

--- src/Server/Infrastructure/Database/Context/Identity/IdentityNoOpEmailSender.cs
+++ src/Server/Infrastructure/Database/Context/Identity/IdentityNoOpEmailSender.cs
@@ -1,20 +1,15 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 
-namespace K7.Server.Infrastructure.Database.Context.Identity
+namespace K7.Server.Infrastructure.Database.Context.Identity;
+
+public sealed class IdentityNoOpEmailSender : IEmailSender<ApplicationUser>
 {
-    // Remove the "else if (EmailSender is IdentityNoOpEmailSender)" block from RegisterConfirmation.razor after updating with a real implementation.
-    public sealed class IdentityNoOpEmailSender : IEmailSender<ApplicationUser>
-    {
-        private readonly IEmailSender emailSender = new NoOpEmailSender();
+    public Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink) =>
+        Task.CompletedTask;
 
-        public Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink) =>
-            emailSender.SendEmailAsync(email, "Confirm your email", $"Please confirm your account by <a href='{confirmationLink}'>clicking here</a>.");
+    public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink) =>
+        Task.CompletedTask;
 
-        public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink) =>
-            emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password by <a href='{resetLink}'>clicking here</a>.");
-
-        public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode) =>
-            emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password using the following code: {resetCode}");
-    }
+    public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode) =>
+        Task.CompletedTask;
 }
